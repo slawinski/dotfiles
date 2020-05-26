7d84d3a37297d763ac2ceef7c154d33e0b07b3e6
piotr-s <piotr.slawinski1@gmail.com>
2020-05-26 15:45:11 +0200

    Add prettier config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
   root: true,
   parserOptions: {
     // parser moved to parser options to fix 'use the latest vue-eslint-parser' error
-    parser: "babel-eslint",
-    sourceType: "module",
+    parser: 'babel-eslint',
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -12,47 +12,47 @@ module.exports = {
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: [
-    "standard",
-    "plugin:vue/recommended", // required to lint *.vue files
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "prettier",
-    "prettier/vue",
+    'standard',
+    'plugin:vue/recommended', // required to lint *.vue files
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'prettier',
+    'prettier/vue',
   ],
   plugins: [],
   rules: {
     // add your custom rules here
-    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0, // allow debugger during development
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "arrow-parens": 0, // allow paren-less arrow functions
-    "generator-star-spacing": 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, // allow debugger during development
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': 0, // allow paren-less arrow functions
+    'generator-star-spacing': 0,
     indent: [2, 4, { SwitchCase: 1 }], // Indentation set to 2
-    "no-tabs": 0, // Give you possibility to use tabs not only on the beginning of line.
-    semi: [2, "always"], // Semicolon at the end of each statement
-    "space-before-function-paren": [2, "never"], // No space between name of the function and brackets.
-    "vue/html-indent": ["error", 4],
-    "vue/html-closing-bracket-newline": [
-      "error",
+    'no-tabs': 0, // Give you possibility to use tabs not only on the beginning of line.
+    semi: [2, 'always'], // Semicolon at the end of each statement
+    'space-before-function-paren': [2, 'never'], // No space between name of the function and brackets.
+    'vue/html-indent': ['error', 4],
+    'vue/html-closing-bracket-newline': [
+      'error',
       {
-        singleline: "never",
-        multiline: "always",
+        singleline: 'never',
+        multiline: 'always',
       },
     ],
-    "vue/name-property-casing": ["error", "kebab-case"],
+    'vue/name-property-casing': ['error', 'kebab-case'],
   },
   overrides: [
     {
       files: [
-        "*.vue",
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '*.vue',
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
       },
       rules: {
-        indent: "off",
-        "vue/script-indent": ["error", 4],
+        indent: 'off',
+        'vue/script-indent': ['error', 4],
       },
     },
   ],
